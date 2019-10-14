ec73532c48859cc45960ca0dc449d36f7333351f
Li.Shangzhi <13680348517@163.com>
2019-10-15 00:56:18 +0800

    监听ApplicationReadyEvent事件，保存插件和元数据

---
+++ framework-discovery/framework-plugin-api/src/main/java/com/ibyte/support/loader/SystemReadyListener.java
@@ -0,0 +1,10 @@
+package com.ibyte.support.loader;
+
+/**
+ * @Description: <监听ApplicationReadyEvent事件，保存插件和元数据>
+ *
+ * @author li.Shangzhi
+ * @Date: 2019-10-15 00:55
+ */
+public class SystemReadyListener {
+}
