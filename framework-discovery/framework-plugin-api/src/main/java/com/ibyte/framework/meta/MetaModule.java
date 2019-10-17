f94cd966f40f6c0df43f15b36c6bafd9a999e4dd
alibaba <13680348511@163.com>
2019-10-17 22:39:31 +0800

    完善模块信息接口

--- framework-discovery/framework-plugin-api/src/main/java/com/ibyte/framework/meta/MetaModule.java
+++ framework-discovery/framework-plugin-api/src/main/java/com/ibyte/framework/meta/MetaModule.java
@@ -7,4 +7,42 @@ package com.ibyte.framework.meta;
  * @Date: 2019-10-17
  */
 public interface MetaModule {
+
+    /**
+     * 取本地模块
+     *
+     * @param moduleName
+     * @return
+     */
+    public static MetaModule localModule(String moduleName) {
+        return LocalMetaContextHolder.get().getModule(moduleName);
+    }
+
+    /**
+     * 读-取字段名
+     *
+     * @return
+     */
+    String getName();
+
+    /**
+     * 读-中文名
+     *
+     * @return
+     */
+    String getLabel();
+
+    /**
+     * 读-MessageKey
+     *
+     * @return
+     */
+    String getMessageKey();
+
+    /**
+     * 读-所在的应用名
+     *
+     * @return
+     */
+    String getAppName();
 }
