8b17782cc6e9c10ca8a4daf0b29131f243543bf4
Li.Shangzhi <13680348517@163.com>
2019-10-18 16:58:00 +0800

    插件扫描监听

---
+++ framework-discovery/framework-plugin-api/src/main/java/com/ibyte/framework/plugin/spi/PluginListener.java
@@ -0,0 +1,50 @@
+package com.ibyte.framework.plugin.spi;
+
+import com.ibyte.framework.support.listener.ClassContext;
+
+/**
+ * @Description: <插件扫描监听>
+ *
+ * @author li.Shangzhi
+ * @Date: 2019-10-18
+ */
+public interface PluginListener {
+
+    /**
+     * 准备
+     */
+    default void prepare() {
+    }
+
+    /**
+     * 当开始扫描类时触发
+     *
+     * @param clazz
+     */
+    default void onClassStart(ClassContext clazz) {
+    }
+
+    /**
+     * 当扫描成员时触发
+     *
+     * @param clazz
+     * @param member
+     */
+    default void onMember(ClassContext clazz, MemberContext member) {
+    }
+
+    /**
+     * 当结束扫描类时触发
+     *
+     * @param clazz
+     */
+    default void onClassEnd(ClassContext clazz) {
+    }
+
+    /**
+     * 完成并保存
+     */
+    default void save() {
+    }
+
+}
