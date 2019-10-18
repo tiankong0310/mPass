67344bf5c4c546e2c9f0a4d02fd50918663182cb
徐贤同 <xiantong.xu@skyroam.com>
2019-10-18 14:45:17 +0800

    设计摘要信息

---
+++ framework-discovery/framework-plugin-api/src/main/java/com/ibyte/framework/meta/MetaSummary.java
@@ -0,0 +1,24 @@
+package com.ibyte.framework.meta;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @Description: <设计摘要信息>
+ *
+ * @author li.Shangzhi
+ * @Date: 2019-10-18
+ */
+@Getter
+@Setter
+@ToString
+public class MetaSummary {
+    private String id;
+
+    private String label;
+
+    private String messageKey;
+
+
+}
