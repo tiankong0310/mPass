8daef59ed9a27941dabd124db3d688b191c1cd71
Li.Shangzhi <13680348517@163.com>
2019-10-22 14:31:10 +0800

    Entity信息扫描填充->调用AIP保存配置

--- framework-discovery/framework-plugin-api/src/main/java/com/ibyte/framework/support/listener/impl/MetaEntityScanListener.java
+++ framework-discovery/framework-plugin-api/src/main/java/com/ibyte/framework/support/listener/impl/MetaEntityScanListener.java
@@ -18,6 +18,7 @@ import com.ibyte.framework.support.domain.MetaEntityImpl;
 import com.ibyte.framework.support.domain.MetaPropertyImpl;
 import com.ibyte.framework.support.listener.ClassContext;
 import com.ibyte.framework.support.listener.MemberContext;
+import com.ibyte.framework.support.persistent.DesignElementApi;
 import com.ibyte.framework.support.util.PluginReflectUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
@@ -38,267 +39,266 @@ import java.util.Map;
  */
 @Slf4j
 public class MetaEntityScanListener implements PluginListener {
-	private static final char DOT = '.';
-	private static final String[] DISPLAY_PROPERTY = { "fdName", "fdSubject" };
-	private static final String SERVICE_CLASS = "com.landray.common.core.service.IService";
+    private static final char DOT = '.';
+    private static final String[] DISPLAY_PROPERTY = {"fdName", "fdSubject"};
+    private static final String SERVICE_CLASS = "com.landray.common.core.service.IService";
 
-	@Override
-	public void onClassStart(ClassContext clazz) {
-		MetaEntity ann = clazz.getAnnotation(MetaEntity.class);
-		List<ExtensionImpl> extensions = clazz
-				.getAndRemoveExtensions(Feature.class.getName());
-		if (ann == null && extensions.isEmpty()) {
-			return;
-		}
-		// 找类对应的Entity
-		MetaEntityImpl entity = (MetaEntityImpl) LocalMetaContextHolder.get()
-				.getOrCreateEntity(clazz.getRefClass().getName());
-		// 填充注解
-		if (ann != null) {
-			fillEntityAnnotation(entity, ann);
-		}
-		// 填充Feature
-		for (ExtensionImpl extension : extensions) {
-			addToFeature(entity.getFeatures(), extension);
-		}
-	}
+    @Override
+    public void onClassStart(ClassContext clazz) {
+        MetaEntity ann = clazz.getAnnotation(MetaEntity.class);
+        List<ExtensionImpl> extensions = clazz
+                .getAndRemoveExtensions(Feature.class.getName());
+        if (ann == null && extensions.isEmpty()) {
+            return;
+        }
+        // 找类对应的Entity
+        MetaEntityImpl entity = (MetaEntityImpl) LocalMetaContextHolder.get()
+                .getOrCreateEntity(clazz.getRefClass().getName());
+        // 填充注解
+        if (ann != null) {
+            fillEntityAnnotation(entity, ann);
+        }
+        // 填充Feature
+        for (ExtensionImpl extension : extensions) {
+            addToFeature(entity.getFeatures(), extension);
+        }
+    }
 
-	@Override
-	public void onMember(ClassContext clazz, MemberContext member) {
-		MetaProperty ann = member.getAnnotation(MetaProperty.class);
-		List<ExtensionImpl> extensions = member
-				.getAndRemoveExtensions(Feature.class.getName());
-		if (ann == null && extensions.isEmpty()) {
-			return;
-		}
-		// 查找对应的entity、property
-		String name = member.getPropertyName();
-		if (name == null) {
-			return;
-		}
-		MetaEntityImpl entity = (MetaEntityImpl) LocalMetaContextHolder.get()
-				.getOrCreateEntity(clazz.getRefClass().getName());
-		MetaPropertyImpl property = (MetaPropertyImpl) entity.getProperty(name);
-		if (property == null) {
-			property = new MetaPropertyImpl();
-			property.setName(name);
-			entity.getProperties().put(name, property);
-		}
-		// 填充注解
-		if (ann != null) {
-			fillPropertyAnnotation(property, ann);
-		}
-		// 填充Feature
-		for (ExtensionImpl extension : extensions) {
-			addToFeature(property.getFeatures(), extension);
-		}
-	}
+    @Override
+    public void onMember(ClassContext clazz, MemberContext member) {
+        MetaProperty ann = member.getAnnotation(MetaProperty.class);
+        List<ExtensionImpl> extensions = member
+                .getAndRemoveExtensions(Feature.class.getName());
+        if (ann == null && extensions.isEmpty()) {
+            return;
+        }
+        // 查找对应的entity、property
+        String name = member.getPropertyName();
+        if (name == null) {
+            return;
+        }
+        MetaEntityImpl entity = (MetaEntityImpl) LocalMetaContextHolder.get()
+                .getOrCreateEntity(clazz.getRefClass().getName());
+        MetaPropertyImpl property = (MetaPropertyImpl) entity.getProperty(name);
+        if (property == null) {
+            property = new MetaPropertyImpl();
+            property.setName(name);
+            entity.getProperties().put(name, property);
+        }
+        // 填充注解
+        if (ann != null) {
+            fillPropertyAnnotation(property, ann);
+        }
+        // 填充Feature
+        for (ExtensionImpl extension : extensions) {
+            addToFeature(property.getFeatures(), extension);
+        }
+    }
 
-	/** 填充feature信息 */
-	private void addToFeature(Map<String, Object> features,
-			ExtensionImpl extension) {
-		ExtensionPointImpl point = extension.getPoint();
-		if (point.getConfig() == null) {
-			return;
-		}
-		features.put(point.getConfig(), extension.getConfig());
-	}
+    /** 填充feature信息 */
+    private void addToFeature(Map<String, Object> features,
+                              ExtensionImpl extension) {
+        ExtensionPointImpl point = extension.getPoint();
+        if (point.getConfig() == null) {
+            return;
+        }
+        features.put(point.getConfig(), extension.getConfig());
+    }
 
-	@Override
-	public void save() {
-		// 查找service并填充
-		fillService();
-		// 填充默认值，然后放入持久化队列
-		LocalMetaContextHolder context = LocalMetaContextHolder.get();
-		Map<String, MetaEntityImpl> configs = new HashMap<>(
-				context.getEntities().size());
-		for (Object entity : context.getEntities().values()) {
-			MetaEntityImpl entityImpl = (MetaEntityImpl) entity;
-			fillEntityDefault(entityImpl);
-			if (entityImpl.getModule() != null) {
-				configs.put(entityImpl.getEntityName(), entityImpl);
-			}
-		}
-		// 保存
-		// TODO 调用AIP保存配置
-		// DesignElementApi.get().saveEntities(configs);
-	}
+    @Override
+    public void save() {
+        // 查找service并填充
+        fillService();
+        // 填充默认值，然后放入持久化队列
+        LocalMetaContextHolder context = LocalMetaContextHolder.get();
+        Map<String, MetaEntityImpl> configs = new HashMap<>(
+                context.getEntities().size());
+        for (Object entity : context.getEntities().values()) {
+            MetaEntityImpl entityImpl = (MetaEntityImpl) entity;
+            fillEntityDefault(entityImpl);
+            if (entityImpl.getModule() != null) {
+                configs.put(entityImpl.getEntityName(), entityImpl);
+            }
+        }
+        // 保存
+        DesignElementApi.get().saveEntities(configs);
+    }
 
-	/** 将注解信息填充到Entity */
-	private void fillEntityAnnotation(MetaEntityImpl entity, MetaEntity ann) {
-		if (StringUtils.isNotBlank(ann.messageKey())) {
-			entity.setMessageKey(ann.messageKey());
-		}
-		if (StringUtils.isNotBlank(ann.displayProperty())) {
-			entity.setDisplayProperty(ann.displayProperty());
-		}
-		if (ann.voClass() != Void.class) {
-			entity.setVoName(ann.voClass().getName());
-		}
-	}
+    /** 将注解信息填充到Entity */
+    private void fillEntityAnnotation(MetaEntityImpl entity, MetaEntity ann) {
+        if (StringUtils.isNotBlank(ann.messageKey())) {
+            entity.setMessageKey(ann.messageKey());
+        }
+        if (StringUtils.isNotBlank(ann.displayProperty())) {
+            entity.setDisplayProperty(ann.displayProperty());
+        }
+        if (ann.voClass() != Void.class) {
+            entity.setVoName(ann.voClass().getName());
+        }
+    }
 
-	/** 将注解信息填充到Property */
-	private void fillPropertyAnnotation(MetaPropertyImpl prop,
-			MetaProperty ann) {
-		if (StringUtils.isNotBlank(ann.messageKey())) {
-			prop.setMessageKey(ann.messageKey());
-		}
-		prop.setNotNull(prop.isNotNull() || ann.notNull());
-		prop.setReadOnly(prop.isReadOnly() || ann.readOnly());
-		if (ann.showType() != ShowType.AUTO) {
-			prop.setShowType(ann.showType());
-		}
-	}
+    /** 将注解信息填充到Property */
+    private void fillPropertyAnnotation(MetaPropertyImpl prop,
+                                        MetaProperty ann) {
+        if (StringUtils.isNotBlank(ann.messageKey())) {
+            prop.setMessageKey(ann.messageKey());
+        }
+        prop.setNotNull(prop.isNotNull() || ann.notNull());
+        prop.setReadOnly(prop.isReadOnly() || ann.readOnly());
+        if (ann.showType() != ShowType.AUTO) {
+            prop.setShowType(ann.showType());
+        }
+    }
 
-	/** 将service信息填充到Entity */
-	private void fillService() {
-		Class<?> serviceClass = PluginReflectUtil.classForName(SERVICE_CLASS);
-		if (serviceClass == null) {
-			return;
-		}
-		ApplicationContext ctx = ApplicationContextHolder
-				.getApplicationContext();
-		String[] names = ctx.getBeanDefinitionNames();
-		for (String name : names) {
-			Object bean = ctx.getBean(name);
-			// System.out.println(name + " : " + bean.getClass().getName());
-			if (serviceClass.isAssignableFrom(bean.getClass())) {
-				try {
-					Class<?> entityClass = (Class<?>) invoke(bean,
-							"getEntityClass");
-					Class<?> voClass = (Class<?>) invoke(bean,
-							"getViewObjectClass");
-					MetaEntityImpl meta = (MetaEntityImpl) LocalMetaContextHolder
-							.get().getEntity(entityClass.getName());
-					if (meta != null) {
-						meta.setApiName(PluginReflectUtil
-								.getBeanClassName(bean.getClass().getName()));
-						meta.setVoName(voClass.getName());
-					}
-				} catch (Exception e) {
-					log.error("加载Service信息失败", e);
-				}
-			}
-		}
-	}
+    /** 将service信息填充到Entity */
+    private void fillService() {
+        Class<?> serviceClass = PluginReflectUtil.classForName(SERVICE_CLASS);
+        if (serviceClass == null) {
+            return;
+        }
+        ApplicationContext ctx = ApplicationContextHolder
+                .getApplicationContext();
+        String[] names = ctx.getBeanDefinitionNames();
+        for (String name : names) {
+            Object bean = ctx.getBean(name);
+            // System.out.println(name + " : " + bean.getClass().getName());
+            if (serviceClass.isAssignableFrom(bean.getClass())) {
+                try {
+                    Class<?> entityClass = (Class<?>) invoke(bean,
+                            "getEntityClass");
+                    Class<?> voClass = (Class<?>) invoke(bean,
+                            "getViewObjectClass");
+                    MetaEntityImpl meta = (MetaEntityImpl) LocalMetaContextHolder
+                            .get().getEntity(entityClass.getName());
+                    if (meta != null) {
+                        meta.setApiName(PluginReflectUtil
+                                .getBeanClassName(bean.getClass().getName()));
+                        meta.setVoName(voClass.getName());
+                    }
+                } catch (Exception e) {
+                    log.error("加载Service信息失败", e);
+                }
+            }
+        }
+    }
 
-	private Object invoke(Object bean, String getter)
-			throws ReflectiveOperationException {
-		Method method = bean.getClass().getMethod(getter);
-		return method.invoke(bean, new Object[] {});
-	}
+    private Object invoke(Object bean, String getter)
+            throws ReflectiveOperationException {
+        Method method = bean.getClass().getMethod(getter);
+        return method.invoke(bean, new Object[]{});
+    }
 
-	/** 自动填充Entity信息 */
-	private void fillEntityDefault(MetaEntityImpl entity) {
-		String simpleName = entity.getEntityName();
-		int index = simpleName.lastIndexOf(DOT);
-		if (index == -1) {
-			return;
-		}
-		entity.setModule(LocalMetaContextHolder.get()
-				.matchModule(entity.getEntityName()));
-		// KmReviewMain -> kmReviewMain
-		simpleName = StringUtils.uncapitalize(simpleName.substring(index + 1));
-		// messageKey/label
-		if (entity.getMessageKey() == null) {
-			if (entity.getModule() != null) {
-				String messageKey = StringHelper.join(entity.getModule(),
-						":table.", simpleName);
-				String label = ResourceUtil.getString(messageKey);
-				if (label != null) {
-					entity.setMessageKey(messageKey);
-					entity.setLabel(label);
-				}
-			}
-		} else {
-			entity.setLabel(ResourceUtil.getString(entity.getMessageKey()));
-		}
-		// displayProperty
-		if (entity.getDisplayProperty() == null) {
-			for (String displayProperty : DISPLAY_PROPERTY) {
-				if (entity.getProperty(displayProperty) != null) {
-					entity.setDisplayProperty(displayProperty);
-					break;
-				}
-			}
-		}
-		// feature
-		tranFeatures(entity.getFeatures());
-		// properties
-		fillPropertyDefault(entity, simpleName);
-	}
+    /** 自动填充Entity信息 */
+    private void fillEntityDefault(MetaEntityImpl entity) {
+        String simpleName = entity.getEntityName();
+        int index = simpleName.lastIndexOf(DOT);
+        if (index == -1) {
+            return;
+        }
+        entity.setModule(LocalMetaContextHolder.get()
+                .matchModule(entity.getEntityName()));
+        // KmReviewMain -> kmReviewMain
+        simpleName = StringUtils.uncapitalize(simpleName.substring(index + 1));
+        // messageKey/label
+        if (entity.getMessageKey() == null) {
+            if (entity.getModule() != null) {
+                String messageKey = StringHelper.join(entity.getModule(),
+                        ":table.", simpleName);
+                String label = ResourceUtil.getString(messageKey);
+                if (label != null) {
+                    entity.setMessageKey(messageKey);
+                    entity.setLabel(label);
+                }
+            }
+        } else {
+            entity.setLabel(ResourceUtil.getString(entity.getMessageKey()));
+        }
+        // displayProperty
+        if (entity.getDisplayProperty() == null) {
+            for (String displayProperty : DISPLAY_PROPERTY) {
+                if (entity.getProperty(displayProperty) != null) {
+                    entity.setDisplayProperty(displayProperty);
+                    break;
+                }
+            }
+        }
+        // feature
+        tranFeatures(entity.getFeatures());
+        // properties
+        fillPropertyDefault(entity, simpleName);
+    }
 
-	/** 自动填充Property信息 */
-	private void fillPropertyDefault(MetaEntityImpl entity, String simpleName) {
-		Class<?> viewClass = StringUtils.isBlank(entity.getVoName()) ? null
-				: PluginReflectUtil.classForName(entity.getVoName());
-		for (Object prop : entity.getProperties().values()) {
-			MetaPropertyImpl propImp = (MetaPropertyImpl) prop;
-			// messageKey/label
-			if (propImp.getMessageKey() == null) {
-				if (entity.getModule() != null) {
-					String messageKey = StringHelper.join(entity.getModule(),
-							':', simpleName, DOT, propImp.getName());
-					String label = ResourceUtil.getString(messageKey);
-					if (label != null) {
-						propImp.setMessageKey(messageKey);
-						propImp.setLabel(label);
-					}
-				}
-				if (propImp.getMessageKey() == null) {
-					String messageKey = StringHelper.join("entity.", simpleName,
-							DOT, propImp.getName());
-					String label = ResourceUtil.getString(messageKey);
-					if (label != null) {
-						propImp.setMessageKey(messageKey);
-						propImp.setLabel(label);
-					}
-				}
+    /** 自动填充Property信息 */
+    private void fillPropertyDefault(MetaEntityImpl entity, String simpleName) {
+        Class<?> viewClass = StringUtils.isBlank(entity.getVoName()) ? null
+                : PluginReflectUtil.classForName(entity.getVoName());
+        for (Object prop : entity.getProperties().values()) {
+            MetaPropertyImpl propImp = (MetaPropertyImpl) prop;
+            // messageKey/label
+            if (propImp.getMessageKey() == null) {
+                if (entity.getModule() != null) {
+                    String messageKey = StringHelper.join(entity.getModule(),
+                            ':', simpleName, DOT, propImp.getName());
+                    String label = ResourceUtil.getString(messageKey);
+                    if (label != null) {
+                        propImp.setMessageKey(messageKey);
+                        propImp.setLabel(label);
+                    }
+                }
+                if (propImp.getMessageKey() == null) {
+                    String messageKey = StringHelper.join("entity.", simpleName,
+                            DOT, propImp.getName());
+                    String label = ResourceUtil.getString(messageKey);
+                    if (label != null) {
+                        propImp.setMessageKey(messageKey);
+                        propImp.setLabel(label);
+                    }
+                }
 
-			} else {
-				propImp.setLabel(
-						ResourceUtil.getString(propImp.getMessageKey()));
-			}
-			// showType
-			if (propImp.getShowType() == null
-					|| propImp.getShowType() == ShowType.AUTO) {
-				if (propImp.isLazy() || propImp.isCollection()
-						|| MetaConstant.isLob(propImp.getType())) {
-					propImp.setShowType(ShowType.DETAIL_ONLY);
-				} else {
-					propImp.setShowType(ShowType.ALWAYS);
-				}
-			}
-			if (viewClass != null) {
-				if (BeanUtils.getPropertyDescriptor(viewClass,
-						propImp.getName()) == null) {
-					propImp.setShowType(ShowType.NONE);
-				}
-			}
-			// feature
-			tranFeatures(propImp.getFeatures());
-		}
-	}
+            } else {
+                propImp.setLabel(
+                        ResourceUtil.getString(propImp.getMessageKey()));
+            }
+            // showType
+            if (propImp.getShowType() == null
+                    || propImp.getShowType() == ShowType.AUTO) {
+                if (propImp.isLazy() || propImp.isCollection()
+                        || MetaConstant.isLob(propImp.getType())) {
+                    propImp.setShowType(ShowType.DETAIL_ONLY);
+                } else {
+                    propImp.setShowType(ShowType.ALWAYS);
+                }
+            }
+            if (viewClass != null) {
+                if (BeanUtils.getPropertyDescriptor(viewClass,
+                        propImp.getName()) == null) {
+                    propImp.setShowType(ShowType.NONE);
+                }
+            }
+            // feature
+            tranFeatures(propImp.getFeatures());
+        }
+    }
 
-	/** 将Feature转换成实现类 */
-	private void tranFeatures(Map<String, Object> features) {
-		if (features.isEmpty()) {
-			return;
-		}
-		List<String> keys = new ArrayList<>(features.keySet());
-		for (String key : keys) {
-			Class<?> clazz = PluginReflectUtil.classForName(key);
-			if (clazz == null) {
-				continue;
-			}
-			Object feature = features.get(key);
-			if (feature instanceof JSONObject && clazz != JSONObject.class) {
-				try {
-					features.put(key, JsonUtil.convert(feature, clazz));
-				} catch (Exception e) {
-					log.error("无法装配Feature：" + key, e);
-				}
-			}
-		}
-	}
+    /** 将Feature转换成实现类 */
+    private void tranFeatures(Map<String, Object> features) {
+        if (features.isEmpty()) {
+            return;
+        }
+        List<String> keys = new ArrayList<>(features.keySet());
+        for (String key : keys) {
+            Class<?> clazz = PluginReflectUtil.classForName(key);
+            if (clazz == null) {
+                continue;
+            }
+            Object feature = features.get(key);
+            if (feature instanceof JSONObject && clazz != JSONObject.class) {
+                try {
+                    features.put(key, JsonUtil.convert(feature, clazz));
+                } catch (Exception e) {
+                    log.error("无法装配Feature：" + key, e);
+                }
+            }
+        }
+    }
 }
