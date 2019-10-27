1c7e96f3725e8acf820b3c97b95dc10c4b2265fb
Li.Shangzhi <13680348517@163.com>
2019-10-27 23:00:48 +0800

    所有常用的Controller组合

---
+++ common/common-core/src/main/java/com/ibyte/common/core/controller/CombineController.java
@@ -0,0 +1,15 @@
+package com.ibyte.common.core.controller;
+
+import com.ibyte.common.core.api.IApi;
+import com.ibyte.common.core.dto.IViewObject;
+
+/**
+ * 所有常用的Controller组合
+ *
+ * @author ibyte
+ */
+public interface CombineController<A extends IApi<V>, V extends IViewObject>
+		extends MetaController<A, V>, CrudController<A, V>,
+		DeleteAllController<A, V>, ListController<A, V> {
+
+}
