33a595350d0a7de87164b9703497a95db43f0040
Li.Shangzhi <13680348517@163.com>
2019-10-28 23:58:49 +0800

    Hibernate 二级缓存/准备阶段

---
+++ common/common-core/src/main/java/com/ibyte/component/jpa/RedissonCacheRegionFactory.java
@@ -0,0 +1,41 @@
+package com.ibyte.component.jpa;
+
+import com.ibyte.framework.support.ApplicationContextHolder;
+import org.hibernate.boot.spi.SessionFactoryOptions;
+import org.hibernate.cache.CacheException;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.codec.SnappyCodec;
+import org.redisson.config.Config;
+import org.redisson.hibernate.RedissonRegionFactory;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Map;
+
+/**
+ * @Description: <Hibernate 二级缓存>
+ *
+ * @author li.Shangzhi
+ * @Date: 2019年10月28日 23:57:13
+ */
+public class RedissonCacheRegionFactory extends RedissonRegionFactory {
+
+    /**
+     * 上下文
+     */
+    private ApplicationContext context;
+    /**
+     * redisson配置
+     */
+    private Config defaultConfig;
+
+    /**
+     * 准备阶段
+     */
+    @Override
+    protected void prepareForUse(SessionFactoryOptions settings, Map properties) throws CacheException {
+        this.context = ApplicationContextHolder.getApplicationContext();
+        this.defaultConfig = context.getBean(Config.class);
+        super.prepareForUse(settings, properties);
+    }
+}
