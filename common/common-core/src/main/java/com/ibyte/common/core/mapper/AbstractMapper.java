305240ba1278c25157939747292e207090701a24
Li.Shangzhi <13680348517@163.com>
2019-10-28 15:32:47 +0800

    Entity与VO的转换工具默认实现

--- common/common-core/src/main/java/com/ibyte/common/core/mapper/AbstractMapper.java
+++ common/common-core/src/main/java/com/ibyte/common/core/mapper/AbstractMapper.java
@@ -25,94 +25,95 @@ import static com.ibyte.common.util.ReflectUtil.accessable;
  */
 @Slf4j
 public abstract class AbstractMapper {
-	protected static final String EXCEPTION_KEY = "errors.dataTranException";
+    protected static final String EXCEPTION_KEY = "errors.dataTranException";
 
-	/** 输出成功日志 */
-	protected void logSuccess(MapperContext context) {
-		if (log.isDebugEnabled()) {
-			String path = context.path == null ? null
-					: StringHelper.join("[", context.path, "]");
-			log.debug(StringHelper.join(
-					context.source.getClass().getSimpleName(), ".",
-					context.propName, " -> ",
-					context.target.getClass().getSimpleName(), ".",
-					context.propName, path, " : success!"));
-		}
-	}
+    /** 输出成功日志 */
+    protected void logSuccess(MapperContext context) {
+        if (log.isDebugEnabled()) {
+            String path = context.path == null ? null
+                    : StringHelper.join("[", context.path, "]");
+            log.debug(StringHelper.join(
+                    context.source.getClass().getSimpleName(), ".",
+                    context.propName, " -> ",
+                    context.target.getClass().getSimpleName(), ".",
+                    context.propName, path, " : success!"));
+        }
+    }
 
-	/** 处理异常 */
-	protected void handleException(MapperContext context, Exception e) {
-		String message = StringHelper.join(
-				context.source.getClass().getSimpleName(), ".",
-				context.propName, " -> ",
-				context.target.getClass().getSimpleName(), ".",
-				context.propName);
-		message = ResourceUtil.replaceArgs(
-				ResourceUtil.getString(EXCEPTION_KEY), message);
-		throw new KmssRuntimeException(EXCEPTION_KEY, message, e);
-	}
+    /** 处理异常 */
+    protected void handleException(MapperContext context, Exception e) {
+        String message = StringHelper.join(
+                context.source.getClass().getSimpleName(), ".",
+                context.propName, " -> ",
+                context.target.getClass().getSimpleName(), ".",
+                context.propName);
+        message = ResourceUtil.replaceArgs(
+                ResourceUtil.getString(EXCEPTION_KEY), message);
+        throw new KmssRuntimeException(EXCEPTION_KEY, message, e);
+    }
 
-	/**
-	 * 转换上下文
-	 * 
-	 * @author 叶中奇
-	 */
-	static class MapperContext {
-		// 环境参数
+    /**
+     * 转换上下文
+     *
+     * @author 叶中奇
+     */
+    static class MapperContext {
+        // 环境参数
 
-		final boolean langSupport = LangUtil.isSuportEnabled();
-		final boolean inList;
-		final List<String> excludes;
+        final boolean langSupport = LangUtil.isSuportEnabled();
+        final boolean inList;
+        final List<String> excludes;
 
-		// Bean级参数，递归下层时修改
+        // Bean级参数，递归下层时修改
 
-		MetaEntity meta;
-		Object source;
-		Object target;
-		String path;
+        MetaEntity meta;
+        Object source;
+        Object target;
+        String path;
 
-		// 字段级参数，更换字段修改
+        // 字段级参数，更换字段修改
 
-		String propName;
-		PropertyDescriptor targetDesc;
-		MetaProperty metaProp;
+        String propName;
+        PropertyDescriptor targetDesc;
+        MetaProperty metaProp;
 
-		public MapperContext(MetaEntity meta, Object source, Object target,
-				boolean inList, String... ignoreProperties) {
-			this.inList = inList;
-			this.excludes = ignoreProperties == null
-					? Collections.emptyList()
-					: Arrays.asList(ignoreProperties);
+        public MapperContext(MetaEntity meta, Object source, Object target,
+                             boolean inList, String... ignoreProperties) {
+            this.inList = inList;
+            this.excludes = ignoreProperties == null
+                    ? Collections.emptyList()
+                    : Arrays.asList(ignoreProperties);
 
-			this.meta = meta;
-			this.source = source;
-			this.target = target;
-		}
+            this.meta = meta;
+            this.source = source;
+            this.target = target;
+        }
 
-		public MapperContext(MapperContext parent, MetaEntity meta,
-				Object source, Object target) {
-			this.inList = parent.inList;
-			this.excludes = parent.excludes;
+        public MapperContext(MapperContext parent, MetaEntity meta,
+                             Object source, Object target) {
+            this.inList = parent.inList;
+            this.excludes = parent.excludes;
 
-			this.meta = meta;
-			this.source = source;
-			this.target = target;
-			this.path = parent.path == null ? parent.propName
-					: StringHelper.join(parent.path, ".", parent.propName);
-		}
+            this.meta = meta;
+            this.source = source;
+            this.target = target;
+            this.path = parent.path == null ? parent.propName
+                    : StringHelper.join(parent.path, ".", parent.propName);
+        }
 
-		public Object getSourceValue(String name)
-				throws ReflectiveOperationException {
-			PropertyDescriptor desc = BeanUtils
-					.getPropertyDescriptor(source.getClass(), name);
-			if (desc == null) {
-				return null;
-			}
-			Method read = desc.getReadMethod();
-			if (!accessable(read)) {
-				return null;
-			}
-			return read.invoke(source);
-		}
-	}
+        public Object getSourceValue(String name)
+                throws ReflectiveOperationException {
+            PropertyDescriptor desc = BeanUtils
+                    .getPropertyDescriptor(source.getClass(), name);
+            if (desc == null) {
+                return null;
+            }
+            Method read = desc.getReadMethod();
+            if (!accessable(read)) {
+                return null;
+            }
+            return read.invoke(source);
+        }
+    }
 }
+
