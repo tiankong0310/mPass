1400c5b310885a9dfb086326661ed215efa6c47c
Li.Shangzhi <13680348517@163.com>
2019-10-28 16:55:04 +0800

    添加新增,修改校验组

---
+++ common/common-api/src/main/java/com/ibyte/common/core/validation/AddGroup.java
@@ -0,0 +1,10 @@
+package com.ibyte.common.core.validation;
+
+/**
+ * @Description: <add 校验组>
+ *
+ * @author li.Shangzhi
+ * @Date: 2019-10-28
+ */
+public interface AddGroup {
+}
---
+++ common/common-api/src/main/java/com/ibyte/common/core/validation/UpdateGroup.java
@@ -0,0 +1,10 @@
+package com.ibyte.common.core.validation;
+
+/**
+ * @Description: <update 校验组>
+ *
+ * @author li.Shangzhi
+ * @Date: 2019-10-28
+ */
+public interface UpdateGroup {
+}
